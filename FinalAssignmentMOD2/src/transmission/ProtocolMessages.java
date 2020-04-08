ef6babefca45571fefcace07555cabf0eb0d0b9f
Tessa Gerritse <tessa.gerritse@nedap.com>
2020-04-08 14:21:09 +0200

    Added acceptable user input to protocolmessages

---
+++ FinalAssignmentMOD2/src/transmission/ProtocolMessages.java
@@ -0,0 +1,10 @@
+package transmission;
+
+public class ProtocolMessages {
+
+	public static final char UPLOAD = 'u';
+	public static final char DOWNLOAD = 'd';
+	public static final char REMOVE = 'm';
+	public static final char REPLACE = 'p';
+	public static final char LIST = 'l';
+}
