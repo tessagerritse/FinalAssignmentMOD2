97d86b7a00412ef3e27f1b9a4df7c3392a9b5356
Tessa Gerritse <tessa.gerritse@nedap.com>
2020-04-12 22:09:41 +0200

    added method to start correct server-task

--- FinalAssignmentMOD2/src/server/Communicator.java
+++ FinalAssignmentMOD2/src/server/Communicator.java
@@ -6,6 +6,8 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import transmission.ProtocolMessages;
+
 public class Communicator implements Runnable {
 	
 	private FileServer fileServer;
@@ -28,12 +30,42 @@ public class Communicator implements Runnable {
 		try {
 			sendMaxNameLengthAndPortNumbers();
 			while (true) {
-				fileServer.handleUpload();
-				fileServer.handleDownload(clientAddress, clientPort);
-				fileServer.handleRemove();
+				handleClientRequest();				
 			}
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
+		}
+	}
+
+	private void handleClientRequest() throws IOException {
+		byte[] requestByte = new byte[1];		
+		DatagramPacket request = new DatagramPacket(requestByte, requestByte.length);
+		communicationSocket.receive(request);
+		
+		System.out.println("Received request");
+		
+		String command = new String(requestByte).trim();
+		
+		
+		System.out.println("The command is: " + command);
+		
+		switch (command) {
+		case ProtocolMessages.UPLOAD:
+			
+			System.out.println("Commanding server to upload the file");
+			
+			fileServer.handleUpload();
+			break;
+		case ProtocolMessages.DOWNLOAD:
+			fileServer.handleDownload(clientAddress, clientPort);
+			break;
+		case ProtocolMessages.REMOVE:
+			fileServer.handleRemove();
+			break;
+		case ProtocolMessages.LIST:
+			break;
+		case ProtocolMessages.QUIT:
+			break;
 		}
 	}
 
